3c218f238557655cbb2356cb68f3ef5029392d01
G <ruban.g@capgemini.com>
2022-10-22 23:09:50 +0530

    checking for key pair

--- keypair.tf
+++ keypair.tf
@@ -4,6 +4,7 @@
 
 resource "tls_private_key" "oskey" {
   algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "local_file" "myterrakey" {
@@ -11,22 +12,22 @@ resource "local_file" "myterrakey" {
   filename = "./instance_keypair/myterrakey.pem"
 }
 
-data "archive_file" "backup" {
-  type = "zip"
-  # source_file = "./instance_keypair/myterrakey.pem"
-  source_file = tls_private_key.oskey.private_key_pem
-  output_path = "${path.module}/archives/backup.zip"
-}
+# data "archive_file" "backup" {
+#   type = "zip"
+#   # source_file = "./instance_keypair/myterrakey.pem"
+#   # source_file = tls_private_key.oskey.private_key_pem
+#   output_path = "${path.module}/archives/backup.zip"
+# }
 
 resource "aws_key_pair" "key121" {
   key_name   = "myterrakey"
   public_key = tls_private_key.oskey.public_key_openssh
   #   public_key = "${var.kp_pem_file}"
 
-  #   # Create a "myKey.pem" to your computer!! (change to chmod 400 myKey.pem for ssh)
-  #   provisioner "local-exec" {
-  #     command = "echo '${tls_private_key.oskey.private_key_pem}' > ./myterrakey.pem"
-  #   }  
+  # Create a "myterrakey.pem" to your computer!! (change to chmod 400 myKey.pem for ssh)
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.oskey.private_key_pem}' > ./instance_keypair/myterrakey.pem"
+  }
 }
 
 /*
