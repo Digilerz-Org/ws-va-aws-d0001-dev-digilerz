0c7b564eddfa2bb644c19e09f5a3344676c41187
G <ruban.g@capgemini.com>
2022-10-22 23:50:02 +0530

    added ssh port

--- instance-sg.tf
+++ instance-sg.tf
@@ -6,6 +6,13 @@ resource "aws_security_group" "nginx-2-sg" {
   vpc_id = aws_vpc.vpc.id
   tags   = local.common_tags
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # HTTP access from VPC
   ingress {
     from_port   = 80
