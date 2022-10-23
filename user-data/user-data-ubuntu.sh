4e7edbcecad17898145c6ea730d7d88f8a8aa588
G <ruban.g@capgemini.com>
2022-10-23 10:21:46 +0530

    adding script for ansible installation

--- user-data/user-data-ubuntu.sh
+++ user-data/user-data-ubuntu.sh
@@ -1,22 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
 sudo apt update
-sudo apt install nginx -y
-sudo ufw allow 'Nginx HTTP'
-sudo systemctl start nginx
-sudo mkdir -p /var/www/your_domain/html
-sudo chown -R $USER:$USER /var/www/your_domain/html
-sudo chmod -R 755 /var/www/your_domain
-sudo echo " Hello welcome to Server 2 (Ubuntu)" >  /var/www/your_domain/html/index.html
-sudo systemctl restart nginx
-sudo systemctl reload nginx
-sudo systemctl enable nginx
-
-# #apache
-# sudo apt-get update
-# sudo apt-get install -y apache2
-# sudo systemctl start apache2
-# sudo systemctl enable apache2
-# echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-
-
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+sudo usermod -aG root ${USER}
+sudo su - ${USER}
+sudo chown -R ${USER} /etc/ansible
+sudo echo 'localhost ansible_connection=local' >> /etc/ansible/hosts
